a6af9a26e5e7a1e641ac351b60b5ec839200c01f
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-22 14:23:11 +0530

    Update logomaker.py

--- TechnoRobot/modules/logomaker.py
+++ TechnoRobot/modules/logomaker.py
@@ -278,7 +278,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "Techno.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [ᴛᴇᴄʜɴᴏ](@Techno_r_o_bot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [ᴛᴇᴄʜɴᴏ](@YourX)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
